a2c060918f98b5386ff6b23b52ccbdc37df296d0
Egor <reichweitedeswahnsinns@gmail.com>
2021-09-09 11:52:31 +0300

    working prosess about getting age to the object

--- source/content.js
+++ source/content.js
@@ -1,12 +1,22 @@
 chrome.runtime.onMessage.addListener(gotMessage);
+var ageObj = new Object(); // stores frequency of appearing certain age
 
 function gotMessage(message, sender, sendResponse) {
     if (message.url.includes('vk.com/im' && '91050183')) {
         var allQuestionnaires = document.getElementsByClassName('im-mess--text'); // array of all bot's messages
         var currentQuestionnairesText = allQuestionnaires[allQuestionnaires.length - 1].innerText; // text of the last message
-        var age = currentQuestionnairesText.match(/,([^,]+),/)[1];
-        var ageObj = new Object(); // stores frequency of appearing certain age
+        var currentQuestionnairesTextArray = currentQuestionnairesText.split(', ');
+        for (i = 0; i < currentQuestionnairesTextArray.length; i++) {
+            if (isNaN(currentQuestionnairesTextArray[i]) === false) {
+                if (ageObj[currentQuestionnairesTextArray[i]]) {
+                    ageObj[currentQuestionnairesTextArray[i]] += 1;
+                } else {ageObj[currentQuestionnairesTextArray[i]] = 1;}
+            }
+        };
+        // var age = currentQuestionnairesText.match(/,([^,]+),/)[1];
+        
         console.log(currentQuestionnairesText)
-        console.log(age)
+        console.log(ageObj)
+        console.log(currentQuestionnairesTextArray)
     } else {console.log('[error:01] Page does not match')}
 }
